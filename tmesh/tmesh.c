e68151ef7b97b0c702f2bebdd9f0ce619d9fa491
Jason Thorpe <thorpej@me.com>
2024-01-21 12:35:03 -0800

    Supply a sane default value for LTDL_LIBRARY_PATH.

--- tmesh/tmesh.c
+++ tmesh/tmesh.c
@@ -583,6 +583,12 @@ where OPTIONS are:\n\
     }
   }
 
+#ifdef LTDL_LIBRARY_PATH_DEFAULT
+  /* if the user did not provide a library path, put a sane default */
+  if (getenv("LTDL_LIBRARY_PATH") == NULL)
+    setenv("LTDL_LIBRARY_PATH", LTDL_LIBRARY_PATH_DEFAULT, 0);
+#endif
+
   /* initialize libtme: */
   (void) tme_init();
 
